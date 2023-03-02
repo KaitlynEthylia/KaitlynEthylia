a614ac8ed1efa3af7e050db052f11e78c3bb648a
Kaitlyn-Ethylia <kaitlyyn.ethylia@proton.me>
2023-03-02 10:45:42 +0000

    Add windows settings

--- windows/setup.ps1
+++ windows/setup.ps1
@@ -1,7 +1,13 @@
-﻿Set-Location -Path $env:USERPROFILE\Documents
+﻿#======================
+#
+#        SETUP
+#
+#======================
+
+Set-Location -Path $env:USERPROFILE\Documents
 
 # allow script execution
-Set-ExecutionPolicy -Scope Process RemoteSigned
+Set-ExecutionPolicy -Scope Process Unrestricted
 
 # install scoop
 irm get.scoop.sh | iex
@@ -20,10 +26,17 @@ git config --global user.name Kaitlyn-Ethylia
 git config --global user.email kaitlyyn.ethylia@proton.me
 
 # install programmes
-scoop install autohotkey windows-terminal CascadiaCode-NF-Mono starship zoxide qutebrowser neovim fzf lf notepadplusplus
+scoop install autohotkey windows-terminal CascadiaCode-NF-Mono starship zoxide qutebrowser neovim fzf lf notepadplusplus nodejs gcc python neofetch
+pip install --trusted-host pypi.org --trusted-host pypi.python.org --trusted-host files.pythonhosted.org hyfetch
 
 # download configs
 git clone https://github.com/KaitlynEthylia/KaitlynEthylia
+
+#======================
+#
+#     COPY CONFIGS
+#
+#======================
 
 # configure autohotkey
 Copy-Item '.\KaitlynEthylia\windows\autohotkey-shortcut.lnk' -Destination "${env:APPDATA}\Microsoft\Windows\Start Menu\Programs\Startup" -Force
@@ -42,7 +55,40 @@ Copy-Item '.\KaitlynEthylia\starship\.config\starship.toml' -Destination "${env:
 # configure neovim
 Copy-Item '.\KaitlynEthylia\nvim\.config\nvim' -Destination $env:LOCALAPPDATA -Recurse -Force 
 git clone https://github.com/wbthomason/packer.nvim "$env:LOCALAPPDATA\nvim-data\site\pack\packer\start\packer.nvim"
+$env:PATH += "${env:USERPROFILE}\scoop\apps\nodejs\current"
 nvim -c PackerSync
 
 # configure lf
 Copy-Item '.\KaitlynEthylia\lf\.config\lf' -Destination $env:LOCALAPPDATA -Recurse -Force 
+
+# configure hyfetch
+mkdir "${env:USERPROFILE}\.config"
+Copy-Item '.\KaitlynEthylia\hyfetch\hyfetch.json' -Destination "${env:USERPROFILE}\.config"
+
+#======================
+#
+#   WINDOWS SETTINGS
+#
+#======================
+
+# taskbar settings
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name TaskbarSmallIcons -Value 1 -Force
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name ShowCortanaButton -Value 0 -Force
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name Hidden -Value 1 -Force
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search -Name SearchboxTaskbarMode -Value 0 -Force
+
+# show file extensions
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name HideFileExt -Value 0 -Force
+
+# hide desktop icons
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name HideIcons -Value 1 -Force
+
+# auto hide taskbar
+$p = 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StuckRects3'
+$v = (Get-ItemProperty -Path $p).Settings
+$v[8]=3
+Set-ItemProperty -Path $p -Name Settings -Value $v
+
+# restart window manager
+taskkill /f /im explorer.exe
+start explorer.exe
