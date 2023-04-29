6a4242e1b425d6a973e9361b8502e4ca73178082
KaitlynEthylia <kaitlyyn.ethylia@proton.me>
2023-04-29 18:27:43 +0100

    Changes as of 29/04/23

---
+++ hyfetch/.config/neofetch/music1.zsh
@@ -0,0 +1,11 @@
+bold=$(tput bold)
+normal=$(tput sgr0)
+blue=$(tput setaf 12)
+white=$(tput setaf 15)
+
+rawPlayer=$(playerctl --list-all | grep -v 'firefox' | head -1)
+
+title=$(playerctl --player $rawPlayer metadata xesam:title)
+artist=$(playerctl --player $rawPlayer metadata xesam:artist)
+
+printf "$blue$bold$title$normal$white - $blue$bold$artist\n"
---
+++ hypr/Scripts/randbg-hypr.sh
@@ -0,0 +1,7 @@
+Backgrounds=(~/Pictures/Backgrounds/*)
+file=$((0 + RANDOM % ${#Backgrounds[@]}))
+path=${Backgrounds[$file]}
+
+hyprctl hyprpaper preload "$path"
+hyprctl hyprpaper wallpaper "HDMI-A-1,$path"
+hyprctl hyprpaper unload all
